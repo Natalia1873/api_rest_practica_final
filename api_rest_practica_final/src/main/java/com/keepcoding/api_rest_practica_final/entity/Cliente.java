d626fdf8d73b615b7ac5e3db186d40b6cd7782d3
Natalia Tsyleva <tsylevanatalia@gmail.com>
2024-11-03 13:36:48 +0100

    changed entity

--- api_rest_practica_final/src/main/java/com/keepcoding/api_rest_practica_final/entity/Cliente.java
+++ api_rest_practica_final/src/main/java/com/keepcoding/api_rest_practica_final/entity/Cliente.java
@@ -46,6 +46,127 @@ public class Cliente implements Serializable{
 	private LocalDate fecha_nacimiento;
 	
 	
+	
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+
+	public int getCodigo_postal() {
+		return codigo_postal;
+	}
+
+
+
+	public void setCodigo_postal(int codigo_postal) {
+		this.codigo_postal = codigo_postal;
+	}
+
+
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+
+	public LocalDate getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+
+
+	public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+
+
 	/**
 	 * 
 	 */
